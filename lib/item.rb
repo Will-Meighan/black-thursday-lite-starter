8737c8ed0208e2caa7e08d23cdecc32cd4f24feb
William Meighan <williamdmeighan@gmail.com>
2019-12-17 14:44:32 -0700

    Class method refactor

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,9 @@
+require 'csv'
+
 class Item
+
+  @@items = []
+
   attr_reader :id, :name, :description, :unit_price, :merchant_id
 
   def initialize(item_info)
@@ -8,4 +13,25 @@ class Item
     @unit_price = item_info[:unit_price].to_i
     @merchant_id = item_info[:merchant_id].to_i
   end
+
+  def self.create_items(file_path)
+    csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+
+    @@items = csv.map do |row|
+       Item.new(row)
+    end
+  end
+
+  def self.find(id)
+    @@items.find do |item|
+      item.id == id
+    end
+  end
+
+  def self.where(merchant_id)
+    @@items.find_all do |item|
+      item.merchant_id == merchant_id
+    end
+  end
+
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,7 +5,7 @@ require './lib/sales_engine'
 class SalesEngineTest < Minitest::Test
   def setup
     @sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
+      :items     => "./test/fixtures/items_truncated.csv",
       :merchants => "./data/merchants.csv",
     })
   end
@@ -15,13 +15,15 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "./data/items.csv", @sales_engine.item_path
+    assert_equal "./test/fixtures/items_truncated.csv", @sales_engine.item_path
     assert_equal "./data/merchants.csv", @sales_engine.merchant_path
   end
 
-  def test_it_can_create_an_item_collection
-    assert_instance_of ItemCollection, @sales_engine.item_collection
+  def test_it_has_attributes
+    assert_instance_of Array, @sales_engine.items
+    assert_equal 4, @sales_engine.items.length
   end
+
 
   def test_it_can_create_a_merchant_collection
     assert_instance_of MerchantCollection, @sales_engine.merchant_collection
